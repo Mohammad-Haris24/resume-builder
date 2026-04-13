770c3462e3fd6471d5a7310faa4f09e6d2767c96
Mohammmad Haris <148324120+Mohammad-Haris24@users.noreply.github.com>
2026-04-13 11:28:33 +0530

    Update CORS origin to production URL

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.use(express.json());
 app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:5173", // or "*"
+    origin: "https://resume-builder-client-59ys.onrender.com", // or "*"
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
